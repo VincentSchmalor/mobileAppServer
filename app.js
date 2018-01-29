e4ccef62244781b4dc6ef6fcc9ab70e975ddc8c3
Laptop_Vincent <vincent.schmalor@gmail.com>
2018-01-29 15:30:21 +0100

    add CRUD-Operations

--- app.js
+++ app.js
@@ -1,29 +1,70 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser({limit: '7mb'}));
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
 
 //DB setup
 mongoose.connect("mongodb://mongo:27017/test");
 
 var userSchema = mongoose.Schema({
-    name: String
+    firstName: String,
+    lastName: String,
+    userName: {type: String, required: true},
+    password: {type: String, required: true}
 });
-var userModel = mongoose.model('userModel', userSchema);
+var tblUser = mongoose.model('tblUser', userSchema);
 
-app.get('/', function (req, res) {
-    userModel.find(function(err, result){
-        console.log(result);
+/**
+ * Create (a specific) user(s)
+ */
+app.post('/users/create', function (req, res) {
+    if(!req.body) {
+        res.send('Use valid userformat');
+        return;
+    }
+    tblUser.create(req.body, function (err, result) {
+        if(err) console.log(err);
         res.send(result);
     });
 });
 
-app.post('/:username', function (req, res) {
-    var testEntry = new userModel({name: req.params.username});
-    testEntry.save(function () {
-        console.log("gespeichert");
+/**
+ * Read (a specific) user(s)
+ */
+app.post('/users/read', function (req, res) {
+    tblUser.find(req.body, function (err, result) {
+        if(err) console.log(err);
+        res.send(result);
     });
 });
 
+/**
+ * Update a specific user
+ */
+app.patch('/users/update', function (req, res) {
+    tblUser.update(req.body.toupdate, req.body.set, function (err, result) {
+        if(err) console.log(err);
+        res.send(result);
+    });
+});
+
+/**
+ * Delete (a specific) user(s)
+ */
+app.delete('/users/delete', function (req, res) {
+    tblUser.remove(req.body, function (err) {
+        if(err){
+            console.log(err);
+            return;
+        }
+        res.send('Deleted');
+    })
+});
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Server listening on port 3000!');
 });
