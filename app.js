7b1ebae735669f4d5884cd112e32f5c88cb39573
Vincent Schmalor <vincent.schmalor@gmail.com>
2018-01-29 01:25:11 +0100

    make the mongo work

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+
 //DB setup
-mongoose.connect("mongodb://mongo:27017");
+mongoose.connect("mongodb://mongo:27017/test");
+
+var userSchema = mongoose.Schema({
+    name: String
+});
+var userModel = mongoose.model('userModel', userSchema);
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    userModel.find(function(err, result){
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.post('/:username', function (req, res) {
+    var testEntry = new userModel({name: req.params.username});
+    testEntry.save(function () {
+        console.log("gespeichert");
+    });
 });
 
 app.listen(3000, function () {
